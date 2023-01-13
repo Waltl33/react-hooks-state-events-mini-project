03e8d5d36904221caccc553ccf722c1caadfc1c9
Walter <walter.clark24@yahoo.com>
2023-01-13 18:27:39 -0500

    category filter

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ console.log({ CATEGORIES, TASKS });
 function App() {
 
   const [tasks, setTask] = useState(TASKS)
+  const [categories, setCategories] = useState("All")
 
   function updateTasks(selectedTask) {
     console.log(selectedTask)
@@ -17,11 +18,23 @@ function App() {
     console.log(updateTaskList)
     setTask(updateTaskList)
   }
+  function selectCategory(category){
+    console.log(category)
+    setCategories(category)
+  }
+console.log("Categories State", categories)
+
+
+const updatedTask = (tasks.filter(task => tasks.categories === categories))
+    // console.log(updatedTasks)
 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter
+      categories = {CATEGORIES} 
+        getCategory = {selectCategory}
+      />
       <NewTaskForm />
       <TaskList
       tasks = {TASKS}
